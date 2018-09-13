036543a2ed575d727fe28ffb231d22681a1673b8
Ruy Gonzalez <ruygonzalez@Ruys-MacBook.local>
2018-09-13 18:06:12 -0400

    Added basic animations when bill is edited

--- Tipster/ViewController.m
+++ Tipster/ViewController.m
@@ -55,4 +55,27 @@
     self.tipLabel.text = [NSString stringWithFormat:@"$%.2f", tip];
     self.totalLabel.text = [NSString stringWithFormat:@"$%.2f", total];
 }
+
+- (IBAction)onEditingBegin:(id)sender {
+    // animation so that when editing is begun on the bill text field, the text field animates by changing location
+    // wraps move, to animate it
+    [UIView animateWithDuration:0.2 animations:^{
+        self.billField.frame = CGRectMake(self.billField.frame.origin.x, self.billField.frame.origin.y + 30, self.billField.frame.size.width, self.billField.frame.size.height);
+        self.tipLabel.alpha = 0; // fade away
+    }];
+}
+
+- (IBAction)onEditingEnd:(id)sender {
+    // animates the textfield back
+    CGRect newFrame = self.billField.frame;
+    newFrame.origin.y -= 30;
+    
+    // wraps move, to animate it
+    [UIView animateWithDuration:0.2 animations:^{
+        self.billField.frame = newFrame;
+        self.tipLabel.alpha = 1; // fade back in when done editing
+    }];
+    
+}
+
 @end
